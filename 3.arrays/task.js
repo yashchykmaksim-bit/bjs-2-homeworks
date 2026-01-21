e6dfe4ded7fe7fd3be0f5b93eccc284aa3e6d1fe
Maksim <yaschyk.maksim@yandex.ru>
2026-01-21 12:24:50 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0) {
+        return 0;
+    }
+    let genders = users.filter(users => users.gender === gender);
+    let ages = genders.map(gender => gender.age);
+    if (ages.length === 0) {
+        return 0;
+    }
+    let summ = ages.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    let average = summ / genders.length;
+    return average;
 }
